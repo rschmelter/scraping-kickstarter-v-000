1b14bb90357896d71f42b85efd336f546de3927f
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-18 23:40:12 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  <li class="project grid_4">...</li>
   binding.pry
 end
 
-
+create_project_hash
